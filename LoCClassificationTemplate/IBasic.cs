de4729dbb898df32c3aab577e6b87d0e18344d2f
sonAndrew <andrewz.jackson667@gmail.com>
2024-01-11 18:37:32 -0800

    Initial commit

---
+++ LoCClassificationTemplate/IBasic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCCTemplate
+{
+    public interface IBasic
+    {
+        public char? Identifier { get; set; }
+        public string? Title { get; set; }
+        public string FullIdentification { get; }
+        public List<string>? SubClasses { get; set; }
+    }
+}
---
+++ LoCClassificationTemplate/ICaption.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCCTemplate
+{
+    public interface ICaption
+    {
+        public string? FullSubIdentifier { get; set; }
+        public char? Number { get; set; }
+        public string? NumberSpan { get; set; }
+        public string? Name { get; set; }
+        public string? Place { get; set; }
+        public string? TimePeriod { get; set; }
+        public List<string?> BibliogrphicForm { get; set; }
+    }
+}
